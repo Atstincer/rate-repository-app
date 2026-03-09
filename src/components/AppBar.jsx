76ec3fa88f39ef38708749c323580125d0ff20b4
Atstincer <atstincer87@gmail.com>
2026-03-09 10:36:23 +0100

    Exercise 10.16: sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,6 +2,9 @@ import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 import AppBarTab from "./AppBarTab";
+import { useQuery } from "@apollo/client";
+import { ME } from "../graphql/queries";
+import SingOutTab from "./SingOutTab";
 
 const styles = StyleSheet.create({
   container: {
@@ -12,11 +15,15 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data } = useQuery(ME);
+  console.log("data in AppBar", data);
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <AppBarTab text={"Repositories"} link={"/"} />
-        <AppBarTab text={"Sign in"} link={"/SignIn"} />
+        {!data.me && <AppBarTab text={"Sign in"} link={"/SignIn"} />}
+        {data.me && <SingOutTab />}
       </ScrollView>
     </View>
   );
---
+++ src/components/SingOutTab.jsx
@@ -0,0 +1,35 @@
+import { Pressable, StyleSheet } from "react-native";
+import Text from "./Text";
+import theme from "../theme";
+import useAuthStorage from "../hooks/useAuthStorage";
+import { useApolloClient } from "@apollo/client";
+
+const styles = StyleSheet.create({
+  text: {
+    padding: 10,
+    color: theme.appBar.textColor,
+  },
+});
+
+const SingOutTab = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+
+  const onPress = async () => {
+    console.log("tab pressed");
+    const accessToken = await authStorage.getAccessToken();
+    console.log("checking authStorage property", accessToken);
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+  };
+
+  return (
+    <Pressable onPress={onPress}>
+      <Text fontSize={"subheading"} fontWeight={"bold"} style={styles.text}>
+        SignOut
+      </Text>
+    </Pressable>
+  );
+};
+
+export default SingOutTab;
